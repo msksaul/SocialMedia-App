3e1d8e53daa9e38f409a6bf29563ef863c016365
msksaul <64514098+msksaul@users.noreply.github.com>
2020-10-06 17:58:03 -0600

    Image Upload

--- backend-functions/functions/index.js
+++ backend-functions/functions/index.js
@@ -1,17 +1,18 @@
 const functions = require('firebase-functions')
 const app = require('express')()
 
-const FBAuth = require('./util/fbAuth')
+const fbAuth = require('./util/fbAuth')
 
 const { getAllScreams, postOneScream } = require('./handlers/screams')
-const { signup, login } = require('./handlers/users')
+const { signup, login, uploadImage } = require('./handlers/users')
 
 //scream routes
 app.get('/screams', getAllScreams)
-app.post('/scream', FBAuth, postOneScream)
+app.post('/scream', fbAuth, postOneScream)
 
 //users routes
 app.post('/signup', signup)
 app.post('/login', login)
+app.post('/user/image', fbAuth, uploadImage)
 
 exports.api = functions.https.onRequest(app)
