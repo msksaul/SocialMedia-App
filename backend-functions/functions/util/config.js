de901313784d05ac935f853d48df628c04a6563f
msksaul <64514098+msksaul@users.noreply.github.com>
2020-10-06 17:11:50 -0600

    Refactoring

---
+++ backend-functions/functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apiKey: "AIzaSyCKeWDaIYsKHM_XSzBti7P-ml58wYtpIsc",
+  authDomain: "social-media-app-c6a0e.firebaseapp.com",
+  databaseURL: "https://social-media-app-c6a0e.firebaseio.com",
+  projectId: "social-media-app-c6a0e",
+  storageBucket: "social-media-app-c6a0e.appspot.com",
+  messagingSenderId: "817706573148",
+  appId: "1:817706573148:web:bf4e6e73d9295013d17599",
+  measurementId: "G-ZYYM0L96H3"
+};
